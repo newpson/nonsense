8fb05871b20cac2dddb05e947f511db3e3ee81ac
newpson <dk.newpson@gmail.com>
2025-04-07 08:18:53 +0300

    add useful abstractions

--- polyline.cpp
+++ polyline.cpp
@@ -8,7 +8,21 @@
 #include "gnuplot-helpers.h"
 #include "variable.h"
 
-#include <raylib.h>
+struct Vector2D
+{
+    double x;
+    double y;
+
+    Vector2D(const double x = 0.0, const double y = 0.0): x(x), y(y) {}
+    Vector2D operator+(const Vector2D v) { return Vector2D(x + v.x, y + v.y); }
+    Vector2D operator-(const Vector2D v) { return Vector2D(x - v.x, y - v.y); }
+
+    friend std::ostream &operator<<(std::ostream &out, const Vector2D &v)
+    {
+        out << v.x << " " << v.y;
+        return out;
+    }
+};
 
 double eval(
     const std::string &expr,
@@ -19,73 +33,77 @@ double eval(
     return Evaluator::eval(expr, lib);
 }
 
-std::vector<double> subdivide(const std::string &expr,
-                              const Evaluator::library_t &lib,
-                              const double a, const double b)
+std::vector<Vector2D> subdivide(const std::string &expr_x,
+                                const std::string &expr_y,
+                                const Evaluator::library_t &lib,
+                                const bool close,
+                                const double ta = 0.0, const double tb = 1.0, const double h = 0.01)
 {
-    const double h = 0.1;
-    std::vector<double> points;
-    for (double t = a; t <= b; t += h) {
-        points.push_back(eval(expr, lib, t));
+    std::vector<Vector2D> points;
+    for (double t = ta; t <= tb; t += h) {
+        points.push_back(Vector2D(eval(expr_x, lib, t), eval(expr_y, lib, t)));
     }
+    if (close)
+        points.push_back(points.front());
     return points;
 }
 
-int winding_number(const std::vector<double> &values_x,
-                    const std::vector<double> &values_y,
-                    const double x0, const double y0)
+int winding_number(const Vector2D &target, const std::vector<Vector2D> &points)
 {
     int w = 0;
-    for (int i = 0; i < values_x.size() - 1; ++i) {
-        if (values_x[i] >= x0) {
-            if (values_y[i] <= y0 && y0 < values_y[i+1])
+    for (int i = 0; i < points.size() - 1; ++i) {
+        if (points[i].x >= target.x) {
+            if (points[i].y <= target.y && target.y < points[i+1].y)
                 ++w;
-            else if (values_y[i] > y0 && y0 >= values_y[i+1])
+            else if (points[i].y > target.y && target.y >= points[i+1].y)
                 --w;
         }
     }
     return w;
 }
 
-void plot_line(double x1, double y1, double x2, double y2)
+void plot_line(Vector2D &begin, Vector2D &end)
 {
-    const double hx = 0.5; // шаг по x
-    const double hy = 0.5; // шаг по y
-    const double vx = x2 - x1; // вектор приближаемой прямой (x)
-    const double vy = y2 - y1; // вектор приближаемой прямой (y)
-    const double dirx = copysign(1.0, vx); // направление вектора прямой (x)
-    const double diry = copysign(1.0, vy); // направление вектора прямой (y)
-    const double invertor = copysign(1.0, vx*vy); // инверсия осей в чётных квадрантах
+    const Vector2D h(0.5, 0.5);
+    const Vector2D linevector = end - begin;
+    const Vector2D direction(std::copysign(1.0, linevector.x),
+                             std::copysign(1.0, linevector.y));
+    const double invertor = copysign(1.0, linevector.x * linevector.y); // инверсия осей в чётных квадрантах
 
-    const double startx = dirx > 0 ? floor(x1/hx)*hx : ceil(x1/hx)*hx; // начальная ячейка (x)
-    const double starty = diry > 0 ? floor(y1/hy)*hy : ceil(y1/hy)*hy; // начальная ячейка (y)
-    const double endx = dirx > 0 ? floor(x2/hx)*hx : ceil(x2/hx)*hx; // конечная ячейка (x)
-    const double endy = diry > 0 ? floor(y2/hy)*hy : ceil(y2/hy)*hy; // конечная ячейка (y)
+    const Vector2D cell_begin(direction.x > 0 ? std::floor(begin.x/h.x)*h.x : std::ceil(begin.x/h.x)*h.x,
+                              direction.y > 0 ? std::floor(begin.y/h.y)*h.y : std::ceil(begin.y/h.y)*h.y);
+    const Vector2D cell_end(direction.x > 0 ? std::floor(end.x/h.x)*h.x : std::ceil(end.x/h.x)*h.x,
+                            direction.y > 0 ? std::floor(end.y/h.y)*h.y : std::ceil(end.y/h.y)*h.y);
 
-    double x = startx; // вектор приближения (x)
-    double y = starty; // вектор приближения (y)
-
-    int i = 0; // счётчик итераций (для отладки)
-    while ((fabs(x - endx) > hx/1000.0 || fabs(y - endy) > hy/1000.0) && i < 1000) {
-        std::cout << "set object rect from " << x << "," << y << " to " << x + dirx*hx << "," << y + diry*hy << std::endl;
-        x += hx*dirx;
-        y += hy*diry;
-        double D = invertor*((y - y1)*vx - (x - x1)*vy);
+    int i = 0;
+    Vector2D cell = cell_begin;
+    while ((std::abs(cell.x - cell_end.x) > h.x/1000.0
+            || std::abs(cell.y - cell_end.y) > h.y/1000.0) && i < 1000) {
+        std::cout << "set object rect from "
+                  << cell.x << "," << cell.y
+                  << " to "
+                  << cell.x + direction.x*h.x << "," << cell.y + direction.y*h.y
+                  << std::endl;
+        cell.x += h.x*direction.x;
+        cell.y += h.y*direction.y;
+        const double D = invertor*((cell.y - begin.y)*linevector.x - (cell.x - begin.x)*linevector.y);
         if (D > 0)
-            y -= hy*diry;
+            cell.y -= h.y*direction.y;
         else if (D < 0)
-            x -= hx*dirx;
+            cell.x -= h.x*direction.x;
         ++i;
     }
-    std::cout << "set object rect from " << x << "," << y << " to " << x + dirx*hx << "," << y + diry*hy << std::endl;
+    std::cout << "set object rect from "
+              << cell.x << "," << cell.y
+              << " to "
+              << cell.x + direction.x*h.x << "," << cell.y + direction.y*h.y
+              << std::endl;
 
-    if (i >= 1000) {
-        std::cerr << "*EXTREME* Overflow at (" << x1 << ", " << y1 << ")->(" << x2 << ", " << y2 << ")" << std::endl;
-        std::cerr << "Last should be (" << endx << ", " << endy << ")" << std::endl;
-    }
+    // if (i >= 1000) {
+    //     std::cerr << "*EXTREME* Overflow at (" << x1 << ", " << y1 << ")->(" << x2 << ", " << y2 << ")" << std::endl;
+    //     std::cerr << "Last should be (" << endx << ", " << endy << ")" << std::endl;
+    // }
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -96,80 +114,28 @@ int main(int argc, char **argv)
     library.insert(Evaluator::default_library.begin(),
                    Evaluator::default_library.end());
 
-    const double ta = std::strtod(argv[3], nullptr);
-    const double tb = std::strtod(argv[4], nullptr);
-    auto values_x = subdivide("t*sin(t)", library, 0.0, 5*3.14);
-    auto values_y = subdivide("t*cos(t)", library, 0.0, 5*3.14);
-    auto frame_x = subdivide("12*cos(t)", library, 0.0, 2*3.15);
-    auto frame_y = subdivide("4*sin(t)", library, 0.0, 2*3.15);
+    auto path = subdivide("t*sin(t)", "t*cos(t)", library, false, 0.0, 5*3.14);
+    // auto frame = subdivide("12*cos(t)", "4*sin(t)", library, true, 0.0, 2*3.15);
+    std::vector<Vector2D> frame = {{0.0, 0.0}, {20.0, 0.0}, {20.0, 20.0}, {0.0, 20.0}, {0.0}, {0.0}};
 
-    for (int i = 0; i < values_x.size() - 1; ++i) {
-        const bool is_inside = (winding_number(frame_x, frame_y, values_x[i], values_y[i]) != 0 &&
-                                winding_number(frame_x, frame_y, values_x[i+1], values_y[i+1]) != 0);
+    for (int i = 0; i < path.size() - 1; ++i) {
+        const bool is_inside = (winding_number(path[i], frame) != 0 &&
+                                winding_number(path[i+1], frame) != 0);
         if (is_inside)
-            plot_line(values_x[i], values_y[i], values_x[i+1], values_y[i+1]);
+            plot_line(path[i], path[i+1]);
     }
-    std::cout << "$points << e" << std::endl;
-    for (int i = 0; i < values_x.size(); ++i) {
-        std::cout << values_x[i] << " " << values_y[i] << std::endl;
-    }
+
+    std::cout << "$path << e" << std::endl;
+    for (int i = 0; i < path.size(); ++i)
+        std::cout << path[i] << std::endl;
     std::cout << "e" << std::endl;
     std::cout << "$frame << e" << std::endl;
-    for (int i = 0; i < frame_x.size(); ++i) {
-        std::cout << frame_x[i] << " " << frame_y[i] << std::endl;
-    }
+    for (int i = 0; i < frame.size(); ++i)
+        std::cout << frame[i] << std::endl;
     std::cout << "e" << std::endl;
-    std::cout << "plot $points with lines ls 1, $frame with lines ls 2" << std::endl;
-
+    std::cout << "plot $path with lines ls 1, $frame with lines ls 2" << std::endl;
     int trash;
     std::cin >> trash;
-
-
-
-
-    // ============
-    //    RAYLIB
-    // ============
-
-    // const char * const &expr_x = "(t^3-3*t)*50+200";
-    // const char * const &expr_y = "(t^2-3)*50+100";
-    // auto values_x = subdivide(expr_x, library, -std::sqrt(3), std::sqrt(3));
-    // auto values_y = subdivide(expr_y, library, -std::sqrt(3), std::sqrt(3));
-
-    // InitWindow(400, 200, "Demo");
-    // SetTargetFPS(30);
-
-    // struct Vector2 from;
-    // struct Vector2 to;
-    // while (!WindowShouldClose())
-    // {
-    //     int winding_number = 0;
-    //     double x0 = GetMouseX();
-    //     double y0 = GetMouseY();
-    //     BeginDrawing();
-    //     ClearBackground(RAYWHITE);
-    //     for (int i = 0; i < values_x.size() - 1; ++i) {
-    //         if (values_x[i] >= x0) {
-    //             if (values_y[i] <= y0 && y0 < values_y[i+1])
-    //                 ++winding_number;
-    //             else if (values_y[i] > y0 && y0 >= values_y[i+1])
-    //                 --winding_number;
-    //         }
-    //         from.x = values_x[i];
-    //         from.y = values_y[i];
-    //         to.x = values_x[i+1];
-    //         to.y = values_y[i+1];
-    //         DrawLineEx(from, to, 2.0, RED);
-    //     }
-    //     DrawCircle(x0, y0, 2, BLUE);
-    //     if (winding_number != 0)
-    //         DrawText("inside!", 20, 20, 20, DARKGRAY);
-    //     DrawText(TextFormat("%d", winding_number), 2, 2, 20, DARKGRAY);
-
-    //     EndDrawing();
-    // }
-
-    // CloseWindow();
 
     return 0;
 }
