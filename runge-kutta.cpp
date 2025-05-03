81de1abd6f66ee6dcecc5a267d6146d7025d4901
newpson <dk.newpson@gmail.com>
2025-05-03 22:06:21 +0300

    increase approximation accuracy

--- runge-kutta.cpp
+++ runge-kutta.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <stack>
 
 #include "evaluator.h"
 #include "gnuplot-helpers.h"
@@ -17,16 +18,15 @@ double eval(const std::string &expression,
     return Evaluator::eval(expression, library);
 }
 
-/// Evaluates expression at x+h
 double estimate(const std::string &expression,
                 const Evaluator::library_t &library,
-                const double x, const double y, const double h)
+                const Vector2D &v, const double h)
 {
-    const double k1 = eval(expression, library, x);
-    const double k2 = eval(expression, library, x + h/2.0);
+    const double k1 = eval(expression, library, v.x);
+    const double k2 = eval(expression, library, v.x + h/2.0);
     const double k3 = k2;
-    const double k4 = eval(expression, library, x + h);
-    return y + h/6.0 * (k1 + 2.0*k2 + 2.0*k3 + k4);
+    const double k4 = eval(expression, library, v.x + h);
+    return v.y + h/6.0 * (k1 + 2.0*k2 + 2.0*k3 + k4);
 }
 
 double error(const double y, const double y_half)
@@ -40,59 +40,76 @@ std::vector<Vector2D> integrate(const std::string &expression,
                                 const double ya, double eps, double hmin = 1e-06)
 {
     std::vector<Vector2D> points;
+    std::stack<double> prohibitors;
 
-
+    const double hcrit = 2.0*6.0*hmin;
     const double dir = std::copysign(1.0, b - a);
+    std::cerr << "info: hcrit = " << hcrit << ", dir = " << dir << std::endl;
 
+    const double length_s = b - a;
+    const double length_u = std::abs(length_s);
     double h = 0.0;
-    const double signed_length = b - a;
-    const double length = std::abs(signed_length);
-    if (length < 10.0 * hmin) {
-        if (length < hmin) {
-            std::cerr << "error: range is less than hmin" << std::endl;
+    if (length_u < 10.0 * hcrit) {
+        if (length_u < hcrit) {
+            std::cerr << "error: range is too small OR hmin is too big" << std::endl;
             return points;
         }
-        h = dir * hmin;
+        h = dir * hcrit;
     } else {
-        h = signed_length/10.0;
+        h = length_s / 10.0;
     }
+    std::cerr << "info: init h = " << h << std::endl;
 
-    double x = a;
-    double y = ya;
+    Vector2D cur(a, ya);
     // points.push_back(Vector2D(x, y));
-    std::cout << Vector2D(x, y) << std::endl;
+    std::cout << cur << std::endl;
 
-    double signed_remain = b - x;
-    double remain = std::abs(signed_remain);
-    while (dir * signed_remain > 0) {
-        if (remain < 2.0*std::abs(h)) {
-            h = signed_remain;
-            if (remain >= 2.0*hmin)
+    double remain_s = b - cur.x;
+    double remain_u = std::abs(remain_s);
+    while (dir * remain_s > 0) {
+        if (remain_u < 2.0*std::abs(h)) {
+            std::cerr << "warning: remain is low" << std::endl;
+            if (remain_u < hcrit) { // can't happen (theoretically)
+                std::cerr << "error: failed to reach the end of the range" << std::endl;
+                return points;
+            }
+            h = remain_s;
+            if (remain_u >= 2.0*hcrit)
                 h /= 2.0;
         }
 
-        double y_next = estimate(expression, library, x, y, h);
-        double y_next_half = estimate(expression, library, x, y, h/2.0);
-        while (error(y_next, y_next_half) > eps && std::abs(h) >= 2.0*hmin) {
+        // TODO approximation stack
+        double y = estimate(expression, library, cur, h);
+        double y_half = estimate(expression, library, cur, h/2.0);
+        while (error(y, y_half) < eps/16.0) {
+            h *= 2.0;
+            y_half = y;
+            y = estimate(expression, library, cur, h);
+            std::cerr << "info: *2, h = " << h << ", eps = " << error(y, y_half) << std::endl;
+        }
+        while (error(y, y_half) > eps && std::abs(h) >= 2.0*hcrit) {
             h /= 2.0;
-            y_next = y_next_half;
-            y_next_half = estimate(expression, library, x, y, h/2.0);
+            y = y_half;
+            y_half = estimate(expression, library, cur, h/2.0);
+            std::cerr << "info: /2, h = " << h << ", eps = " << error(y, y_half) << std::endl;
         }
 
-        std::cerr << "info: h = " << h << ", eps = " << error(y_next, y_next_half) << std::endl;
-        y = y_next;
-        x += h;
-        std::cout << Vector2D(x, y) << std::endl;
-        // points.push_back(Vector2D(x, y));
+        cur.y = y;
+        cur.x += h;
+        std::cout << cur << std::endl;
+        std::cerr << "info: fin, h = " << h << ", eps = " << error(y, y_half) << std::endl;
+        // // points.push_back(Vector2D(x, y));
 
-        signed_remain = b - x;
-        remain = std::abs(signed_remain);
+        remain_s = b - cur.x;
+        remain_u = std::abs(remain_s);
+
+        std::cerr << "info: ---" << std::endl;
     }
 
     return points;
 }
 
-double antiderivative(double x)
+double antiderivative(const double x)
 {
     return -std::sin(2.0*x)/4.0 + x*std::log(x) - x/2.0;
 }
@@ -103,9 +120,9 @@ int main(int argc, char **argv)
     library.insert(Evaluator::default_library.begin(),
                    Evaluator::default_library.end());
 
-    const double A = 0.101;
-    const double B = 0.100;
-    auto points = integrate("sin(x)^2 + log(x)", library, A, B, antiderivative(A), 1e-03, 1e-03);
+    const double A = 0.1;
+    const double B = 0.9;
+    auto points = integrate("sin(x)^2 + log(x)", library, A, B, antiderivative(A), 1e-03);
     // for (const auto &point: points)
     //     std::cout << point << std::endl;
 }
