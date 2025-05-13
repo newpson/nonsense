c9906ee793baec4c4c1fe8a5b9f2b3b0f93209d5
newpson <dk.newpson@gmail.com>
2025-05-13 21:47:01 +0300

    add useful functions to math.h

--- math.h
+++ math.h
@@ -6,6 +6,7 @@ struct Vector2D
     double y;
 
     Vector2D(const double x = 0.0, const double y = 0.0): x(x), y(y) {}
+    Vector2D(const Vector2D &v): x(v.x), y(v.y) {}
     Vector2D operator+(const Vector2D &v) const { return Vector2D(x + v.x, y + v.y); }
     Vector2D operator-(const Vector2D &v) const { return Vector2D(x - v.x, y - v.y); }
 
@@ -22,3 +23,45 @@ struct Vector2D
         return out;
     }
 };
+
+inline double min(const double a, const double b)
+{
+    return a < b ? a : b;
+}
+
+inline double min(const double a, const double b, const double c)
+{
+    return min(min(a, b), c);
+}
+
+inline double min(const double a, const double b, const double c, const double d)
+{
+    return min(min(min(a, b), c), d);
+}
+
+inline double is_inside(const double x, const double a, const double b)
+{
+    return (x >= a && x <= b);
+}
+
+inline const Vector2D &min(const Vector2D &a, const Vector2D &b)
+{
+    return a.y < b.y ? a : b;
+}
+
+inline const Vector2D &min(const Vector2D &a, const Vector2D &b, const Vector2D &c)
+{
+    return min(min(a, b), c);
+}
+
+inline const Vector2D &min(const Vector2D &a, const Vector2D &b, const Vector2D &c, const Vector2D &d)
+{
+    return min(min(min(a, b), c), d);
+}
+
+bool is_convex(const Vector2D &a, const Vector2D &center, const Vector2D &b)
+{
+    const double delta_ac = a.y - center.y;
+    const double delta_bc = b.y - center.y;
+    return (delta_ac >= 0) && (delta_bc >= 0) && (delta_ac + delta_bc > 0);
+}
